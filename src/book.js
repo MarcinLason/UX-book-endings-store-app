a1e5637e86304744644d1a72a0a5fd3a1e2359c3
Marcin Lason <marcin.lason1@gmail.com>
2018-07-02 19:32:16 +0200

    Add stars, comments and endings on book's view

--- src/book.js
+++ src/book.js
@@ -4,6 +4,36 @@ import image0 from './assets/0.jpg';
 import image1 from './assets/1.jpg';
 import image2 from './assets/2.jpg';
 import image3 from './assets/3.jpg';
+import './books.css';
+import StarRatingComponent from 'react-star-rating-component';
+
+class StarComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            rating: this.props.rate
+        };
+    }
+    onStarClick(nextValue, prevValue, name) {
+        this.setState({rating: nextValue});
+    }
+
+    render() {
+        const { rating } = this.state;
+
+        return (
+            <div>
+                <StarRatingComponent
+                    name="rate1"
+                    starCount={5}
+                    value={rating}
+                    onStarClick={this.onStarClick.bind(this)}
+                />
+            </div>
+        );
+    }
+}
 
 
 class BookParameters extends React.Component {
@@ -22,9 +52,19 @@ class BookParameters extends React.Component {
         }
     }
 
+    prepareComments(comments) {
+        return "elo";
+    }
+
+    prepareEndings(endings) {
+        return "czesc"
+    }
+
     render() {
         var id = (this.props.url).split("/book")[1].replace('/', '');
         var currentBook = books[parseInt(id)];
+        var commentsString = this.prepareComments(currentBook['comments']);
+        var endingsString = this.prepareEndings(currentBook);
         return (
             <div>
                 <div id="bookParameters">
@@ -52,6 +92,28 @@ class BookParameters extends React.Component {
                 </div>
                 <div id="bookImage">
                     <img id="bookImg" src={this.getImageToDisplay(id)} alt="Logo"/>
+                </div>
+                <div id="start">
+                    <StarComponent rate={currentBook['rank']}/>
+                </div>
+                <div>
+                    <textarea id="description-area">{currentBook['description']}</textarea>
+                </div>
+                <div id="com-end">
+                    <div id="comments">
+                        <h1>Komentarze</h1>
+                        <textarea disabled={true} id="comments-area">
+                            {commentsString}
+                        </textarea>
+                    </div>
+                    <div id="endings">
+                        <h1>Zakończenia alternatywne</h1>
+                        <div id="endings-div">
+                            <textarea disabled={true} id="endings-area">
+                                {endingsString}
+                            </textarea>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
